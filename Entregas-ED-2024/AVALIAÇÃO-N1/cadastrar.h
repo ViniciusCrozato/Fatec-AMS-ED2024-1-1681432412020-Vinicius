712e67dfbdab3adace99f954f9db14acdba7a918
ViniciusCrozato <160658562+ViniciusCrozato@users.noreply.github.com>
2024-04-08 15:49:27 -0300

    Update cadastrar.h

--- Entregas-ED-2024/AVALIAÇÃO-N1/cadastrar.h
+++ Entregas-ED-2024/AVALIAÇÃO-N1/cadastrar.h
@@ -1,7 +1,9 @@
+#ifndef CADASTRAR_H
+#define CADASTRAR_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "entrada.h"
 
 struct produto{
   int id;
@@ -17,13 +19,32 @@ Produto *cria(void){
   return NULL;
 }
 
-Produto *insere(Produto *lista, int id, char *nome, int qtd, float preco){
+Produto * insere (Produto * *lista, int id, char nome [], int qtd, float preco){
+  
   Produto * novo = (Produto *) malloc(sizeof(Produto));
+  
   novo->id = id;
   strcpy(novo->nome, nome);
   novo->qtd = qtd;
   novo->preco = preco;
-  novo->prox = lista;
-  return novo;
+  novo->prox = *lista;
+
+  *lista = novo;
+  return 0;
 }
 
+Produto * imprime(Produto * *lista)
+{
+  Produto *p;
+  for(p = *lista; p != NULL; p = p->prox)
+    {
+      printf("id: %d\n", p->id);
+      printf("nome: %s\n", p->nome);
+      printf("quantidade: %d\n", p->qtd);
+      printf("preço: %2.2f\n", p->preco);
+      printf("-----------------------\n");
+    }
+  return 0;
+}
+
+#endif /* CADASTRAR_H */
