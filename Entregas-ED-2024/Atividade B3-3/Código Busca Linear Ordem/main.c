958bb40571ee6417a83f52f144fce89f4d2e6027
ViniciusCrozato <160658562+ViniciusCrozato@users.noreply.github.com>
2024-09-09 20:39:05 -0300

    Add files via upload

---
+++ Entregas-ED-2024/Atividade B3-3/Código Busca Linear Ordem/main.c
@@ -0,0 +1,31 @@
+/*------------------------------------------------------------------------*/
+/*             FATEC-São Caetano do Sul - Estrutura de Dados              */
+/*                      Atividade: Busca Linear em Ordem                  */
+/*             Objetivo: Buscar elemento em um array ordenado             */
+/*                      Autores: Vinicius Crozato                         */
+/*                                                                        */
+/*                                                         Data:08/09/2024*/
+/*------------------------------------------------------------------------*/
+
+#include <stdio.h>
+
+int main() {
+
+    int A[] = {10, 20, 30, 40, 50, 60, 70, 80};
+    int x = 50;
+
+    int i = 0;
+
+    while (i <= sizeof(A) && x >= A[i]){
+
+        if(x == A[i]){
+            printf("Valor encontrado: %d", A[i]);
+          return i;
+        }
+
+        i = i + 1;
+    }
+        printf("Valor não encontrado!");
+        return -1;
+
+}
